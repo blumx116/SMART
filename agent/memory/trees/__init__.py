44830f7628fcb4e7e3e74012b3036a0f753810af
Carter Blum <blumx116@umn.edu>
2020-04-21 20:40:49 -0500

    added random move chance to greedy_maze_agent.py

--- agent/memory/trees/__init__.py
+++ agent/memory/trees/__init__.py
@@ -1,2 +1,3 @@
 from .binary_tree_node import BinaryTreeNode 
 from .binary_tree import BinaryTree
+from .bounded_binary_tree import BoundedBinaryTree
--- agent/simple/greedy_maze_agent.py
+++ agent/simple/greedy_maze_agent.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from env.mazeworld import MazeWorld
+from utils import array_contains, array_random_choice
 
 Point = np.ndarray # np.ndarray[int]: [2,] (y, x)
 Action = np.ndarray # np.ndarray[int]: [4,], onehot
@@ -15,8 +16,14 @@ class GreedyMazeAgent:
         if self.env is None:
             assert env is not None 
         self.env = env
+        self.eps = 0.03
+        self.eps_max = 0.7
+        self.history = []
+        self.n_repeats = 0  
 
     def act(self, state: State, goal: Goal) -> Action:
+        if np.random.uniform() < self.get_rand_chance():
+            return array_random_choice(self.env.actions)
         assert self.env is not None 
         distances = map(
             lambda action: self._distance(self.env._calculate_move(action), goal),
@@ -29,8 +36,16 @@ class GreedyMazeAgent:
             return None 
         return self.env.actions[np.argmin(distance_deltas)]
 
+    def get_rand_chance(self):
+        chance = self.eps * (self.n_repeats + 1)
+        return np.clip(chance, 0, self.eps_max)
+
     def observe(self, reward: float, state: State, goal: Goal) -> None:
-        pass
+        point = self.env._location #this is cheaty, but temporary
+        if array_contains(point, self.history):
+            self.n_repeats += 1
+        else:
+            self.history.append(self.env._location) 
 
     def _distance(self, location: Point, goal: Goal) -> float:
         goal_point: Point = self.env._grid_to_point(goal)

f112b9a2fb417d8243b65fe3a5ec84293bda05f8
Carter Blum <blumx116@umn.edu>
2020-04-29 01:33:19 -0500

    Create NumPyDict

--- misc/utils.py
+++ misc/utils.py
@@ -4,6 +4,7 @@ from typing import List, Iterable, TypeVar, Union, Tuple
 import numpy as np
 
 T = TypeVar("T")
+V = TypeVar("V")
 
 def flatmap(data: Iterable[Iterable[T]]) -> List[T]:
     return [el for l in data for el in l]
@@ -86,3 +87,46 @@ def optional_random(rand_seed: Union[int, np.random.RandomState] = None):
 
 PROJECT_BASE_DIR: str = os.path.dirname(__file__)
 
+class NumPyDict(Generic[T, V]):
+    def __init__(self, dtype=float):
+        self.inner = {}
+        self.dtype = dtype
+
+    def _pack_np(self, key: T) -> T:
+        """
+            converts np.ndarrays to bytes
+            everything else unchanged
+        """
+        if isinstance(key, np.ndarray):
+            return key.tobytes()
+        return key
+
+    def _unpack_np(self, key: T) -> T:
+        """
+            convertes bytes to np.ndarray
+            everything else unchanged
+        """
+        if isinstance(key, bytes):
+            return np.frombuffer(key, dtype=self.dtype)
+        return key
+
+    def __getitem__(self, key: T) -> V:
+        return self.inner[self._pack_np(key)]
+
+    def __setitem__(self, key: T, value: V) -> None:
+        self.inner[self._pack_np(key)] = value
+
+    def __iter__(self):
+        return iter(self.inner)
+
+    def __next__(self):
+        return self.inner.__next__()
+
+    def __contains__(self, key: T) -> bool:
+        return self._pack_np(key) in self.inner 
+
+    def keys(self) -> Iterable[T]:
+        return map(self._unpack_np, self.inner.keys())
+
+    def values(self) -> Iterable[V]:
+        return self.inner.values()

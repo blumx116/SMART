b4f35921bd27717b6d2fb096212e869411bf7791
Carter Blum <blumx116@umn.edu>
2020-05-19 08:19:55 -0500

    wrote memory module for abstract agent

--- agent/memory/i_memory.py
+++ agent/memory/i_memory.py
@@ -2,8 +2,18 @@ from typing import Generic
 
 from interface import Interface
 
-from misc.typevars import State, Action, Reward, Goal, Trajectory
+from misc.typevars import State, Action, Reward, Goal, Trajectory, Environment
+from misc.typevars import TrainSample
 
 class IMemory(Interface, Generic[State, Action, Reward, Goal]):
-    def get_trajectory(self, state: State, goal_node: Node[Goal]) -> Trajectory:
+    def reset(self, env: Environment, state: State, goal: Goal) -> None:
+        pass
+
+    def view(self, state: State, action: Action, reward: Reward) -> None:
+        pass
+
+    def sample_batch(self, count: int) -> List[TrainSample]:
+        pass
+    
+    def get_trajectory(self, goal_node: Node[Goal]) -> Trajectory:
         pass

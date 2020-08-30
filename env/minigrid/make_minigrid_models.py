443d338cfba2bebf9cdbd9b2b7e2c63732820eec
Carter Blum <blumx116@umn.edu>
2020-08-29 22:03:46 -0500

    (untested) wrote code for Q and V models

--- env/minigrid/make_minigrid_models.py
+++ env/minigrid/make_minigrid_models.py
@@ -5,9 +5,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from agent.evaluator import IQModel
+from agent.evaluator import IQModel, IVModel
 from env import IEnvironment
 from misc.typevars import State, Action, Reward, Option, OptionData
+from misc.utils import Stacker
 
 class EncoderModel(nn.Module):
     def __init__(self,
@@ -18,9 +19,10 @@ class EncoderModel(nn.Module):
         self.num_inputs: int = num_inputs
         self.image_shape: Tuple[int, int] = image_shape
         self.device: torch.device = device
-        self.model = nn.Sequential(
-            nn.Conv2d(1, out_channels=n_features, kernel_size=(3, 3), padding=1),
-            nn.ReLU()).to(self.device)
+        stacker = Stacker(input_shape=image_shape)
+        stacker.stack(nn.Conv2d(1, out_channels=n_features, kernel_size=(3, 3), padding=1))
+        stacker.stack(nn.ReLU())
+        self.model, self.output_dim = stacker.get()
 
     def encode(self, option: Option[OptionData]) -> torch.Tensor:
         """
@@ -72,17 +74,71 @@ def StateEncoder(
 
     return model.to(device)
 
-
-class MinigridQModel(IQModel[State, Action, Reward]):
-   def __init__(self,
+def ValueModel(
         input_shape: Tuple[int, int, int],
-        n_features: int,
+        n_feature: int,
         n_layers: int,
         device: torch.device):
-       self.option_encoder: nn.Sequential = EncoderModel(input_shape[:2], n_features * 2, num_inputs=3, device=device)
-       # [Option, Option, Option] => Tensor[dev, f32] : [imx, imy, 2*n_features]
-       self.state_encoder: nn.Sequential = StateEncoder(input_shape, n_features, n_layers, device=device)
-       # Tensor[dev, f32]: input_shape => Tensor[dev, f32] : [imx, imy, n_features]
-       self.value_function =
+    stacker = Stacker(input_shape)
+    shape: List[int] = list(input_shape)
+    for _ in range(n_layers):
+        shape = stacker.stack(nn.Conv2d(shape[1], n_feature, kernel_size=(3,3)))
+        shape = stacker.stack(nn.MaxPool2d(kernel_size=(2, 2), stride=2))
+        shape = stacker.stack(nn.ReLU())
+    shape = stacker.stack(nn.Flatten())
+    stacker.stack(nn.Linear(shape[1], 1))
+    return stacker.get(device)
+
+class ValueModel(nn.Module):
+    def __init__(self,
+            n_options_input: int,
+            image_shape: List[int],
+            n_feature: int,
+            n_layers: int,
+            device: torch.device):
+        self.option_encoder: nn.Sequential = EncoderModel(image_shape[:2], n_feature * 2, n_options_input, device)
+        self.state_encoder: nn.Sequential = StateEncoder(image_shape, n_feature, n_layers, device)
+        state_embed_size: List[int] = Stacker.get_output_shape(image_shape, self.state_encoder)
+        self.value_net: nn.Sequential = ValueModel(state_embed_size, n_feature, n_layers, device)
+        self.n_features: int = n_feature
+
+    def forward(self, state: State, options: List[Option]) -> torch.Tensor:
+        option_embedding: torch.Tensor = self.option_encoder(options)
+        # Tensor : [batch, imx, imy, n_features * 2]
+        state_embedding: torch.Tensor = self.state_encoder(state)
+        # Tensor: [batch, imx, imy, n_features]
+        combined: torch.Tensor = (option_embedding[:, :, :, :self.n_features] * state_embedding)
+        combined += option_embedding[:, :, :, self.n_features:]
+        # Tensor: [batch, imx, imy, n_features]
+        return self.value_net(combined)
+
+class QModel(ValueModel, IQModel[State, Reward, OptionData]):
+    def __init__(self,
+                 image_shape: List[int],
+                 n_feature: int,
+                 n_layers: int,
+                 device: torch.device):
+        super().__init__(3, image_shape, n_feature, n_layers, device)
+
+    def forward(self,
+            state: State,
+            prev_option: Option[OptionData],
+            suboption: Option[OptionData],
+            option: Option[OptionData]) -> torch.Tensor:
+        return super().forward(state, [prev_option, suboption, option])
+
+class VModel(ValueModel, IVModel[State, Reward, OptionData]):
+    def __init__(self,
+                 image_shape: List[int],
+                 n_feature: int,
+                 n_layers: int,
+                 device: torch.device):
+        super().__init__(2, image_shape, n_feature, n_layers, device)
+
+    def forward(self,
+                state: State,
+                prev_option: Option[OptionData],
+                option: Option[OptionData]) -> torch.Tensor:
+        return super().forward(state, [prev_option,  option])
 
 
